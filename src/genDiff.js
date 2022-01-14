54134a8e65466beacb2e253abf83b21d2576baff
Ilya <i.kostin@semrush.com>
2022-01-14 21:06:40 +0300

    fix genDiff, index.tests.js

--- src/genDiff.js
+++ src/genDiff.js
@@ -29,7 +29,7 @@ const genDiff = (file1, file2) => {
     }
   }
   result = JSON.stringify(result, null, 2);
-  return result.replace(/"/g, '');
+  return result.replace(/"/g, '').replace(/,/g, '');
 };
 
 export default genDiff;
