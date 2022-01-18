77c0d30ab8ab6363dbdb0748a3deec2f1b1cd086
Ilya <i.kostin@semrush.com>
2022-01-18 14:30:20 +0300

    change index.tests.js

--- __tests__/index.tests.js
+++ __tests__/index.tests.js
@@ -9,6 +9,8 @@ const __dirname = dirname(__filename);
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
 
-test('check for correct diff', () => {
-  expect(genDiff('file1.json', 'file2.json')).toEqual(readFile('expected_file.txt'));
+describe('check for correct diff', () => {
+  it('compare files', () => {
+    expect(genDiff('file1.json', 'file2.json')).toEqual(readFile('expected_file.txt'));
+  });
 });
