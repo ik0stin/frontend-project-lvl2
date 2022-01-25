32e93e1b95323f3987940341c8179ea1ebb544f8
Ilya <i.kostin@semrush.com>
2022-01-25 14:22:37 +0300

    fix parsers.js, genDiff.js

--- src/parsers.js
+++ src/parsers.js
@@ -1,6 +1,7 @@
-import fs from 'fs';
+// import fs from 'fs';
 import yaml from 'js-yaml';
 import path from 'path';
+import { readFileSync } from 'fs';
 
 // let parse;
 // if (format === '') {
@@ -13,7 +14,8 @@ import path from 'path';
 
 const parser = (file) => {
   const format = path.extname(file);
-  const data = fs.readSync(file);
+  // const data = fs.readSync(file);
+  const data = readFileSync(path.resolve(file), 'utf8');
   if (format === '') {
     return JSON.parse(data);
   }
