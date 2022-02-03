919a41ef19d5995149309641aed9f11b78973e37
Ilya <i.kostin@semrush.com>
2022-02-03 19:40:44 +0300

    fix test

--- __tests__/index.tests.js
+++ __tests__/index.tests.js
@@ -9,17 +9,19 @@ const __dirname = dirname(__filename);
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
 
+const tests = [
+  ['file1.json', 'file2.json', 'expected_file.txt'],
+  ['file1.yml', 'file2.yml', 'expected_file.txt'],
+  ['file11.json', 'file22.json', 'expected_file_2.txt'],
+  ['file11.yml', 'file22.yml', 'expected_file_2.txt'],
+];
+
 describe('check for correct diff', () => {
-  it('compare files json', () => {
-    expect(genDiff('file1.json', 'file2.json')).toEqual(readFile('expected_file.txt'));
-  });
-  it('compare deep files json', () => {
-    expect(genDiff('file11.json', 'file22.json')).toEqual(readFile('expected_file_2.txt'));
-  });
-  it('compare files yml', () => {
-    expect(genDiff('file1.yml', 'file2.yml')).toEqual(readFile('expected_file.txt'));
-  });
-  it('compare deep files yml', () => {
-    expect(genDiff('file11.yml', 'file22.yml')).toEqual(readFile('expected_file_2.txt'));
+  test.each(tests)('Compare files', (firstArg, secondArg, expectedResult) => {
+    const firstFile = getFixturePath(firstArg);
+    const secondFile = getFixturePath(secondArg);
+    const getResult = readFile(expectedResult);
+    const result = genDiff(firstFile, secondFile);
+    expect(result).toEqual(getResult);
   });
 });
--- bin/gendiff.js
+++ bin/gendiff.js
@@ -7,9 +7,9 @@ const program = new Command();
 program
   .version('0.0.1', '-V, --version', 'output the version number')
   .description('Compares two configuration files and shows a difference.')
-  .option('-f, --format [type]', 'output format')
+  .option('-f, --format [type]', 'output format', 'stylish')
   .arguments('<filepath1> <filepath2>')
-  .action((filepath1, filepath2) => {
-    console.log(genDiff(filepath1, filepath2));
+  .action((filepath1, filepath2, { format }) => {
+    console.log(genDiff(filepath1, filepath2, format));
   });
 program.parse();
